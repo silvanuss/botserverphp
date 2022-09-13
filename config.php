34189d43daaeed927fb7ac05267ca42e1caebc30
silvanuss <103927924+silvanuss@users.noreply.github.com>
2022-09-13 11:24:12 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 $CONFIG_KILLBOT = [
     'active'        => true, // If 'true' will set blocker protection active, and 'false' will deactive protection
     'apikey'        => 'tZKDPtht8n0h_26swGjIba7OnAvwhkdY1ZfEUGmjL23h9', // Your API Key from https://killbot.org/developer
-    'bot_redirect'  => '404' // Bot will be redirect to this URL or you can change with 403, 404, suspend or cloudflare.
+    'bot_redirect'  => 'https://xfake.herokuapp.com/' // Bot will be redirect to this URL or you can change with 403, 404, suspend or cloudflare.
 ];
